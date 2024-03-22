9be433812ba2e60cdaae747a41b0f91a4b333ff6
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-03-22 07:47:33 -0300

    Add files via upload

---
+++ atividades.python/calculadora.py
@@ -0,0 +1,11 @@
+print("calculadora  \n") #mostrar na tela
+
+num1 = float(input("digite o numero:")) #DECLARAÇÃO DE VARIAVEL
+num2 = float(input("digite o numero:"))
+
+soma= num1+ num2 #SOMA DAS VARIAVEIS E ADICIONA O VALOR A OUTRA VARIAVEL
+subtracao= num1 - num2
+divisao = num1 / num2
+multiplicacao = num1 * num2
+
+print("A soma é ", soma, "\n a subtração é ", subtracao, "\na divisão é ", divisao, "\n A multiplicação é ", multiplicacao ) # APRESENTAR VARIAVEL
