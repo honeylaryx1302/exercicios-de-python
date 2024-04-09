88bad0939c1664095bc45f8195a6beec3f6be44b
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-04-09 10:10:03 -0300

    Add files via upload

--- atividades.python/app.py
+++ atividades.python/app.py
@@ -1,26 +1,49 @@
 import os
 
-print ("""sabor express
-       """)
+restaurantes = ['pizza','xp']
 
-print('1. cadastrar restaurante')
-print('2. listar resturante ')
-print('3. ativar restaurante')
-print('4. sair')
+def exibir_nome_do_programa():
+ print ("""sabor express
+""")
+def exibir_opcoes():
+ print('1. cadastrar restaurante')
+ print('2. listar resturante ')
+ print('3. ativar restaurante')
+ print('4. sair')
 
+def finalizar_app():
+    exibir_subtitulo('finalizar app')
+
+def voltar_ao_menu_principal():
+   input('\n Digite uma tecla "enter"para voltar ao menu principal')
+   main()
+           
+     
 opcao_escolhida = int(input('escolha uma opção'))
 #print('você escolheu a opção', opção_escolhida)
 #print(f' vocêescolheu a opção {opção_escolhida}')
 
-def finalizar_app():
-    os.system('cls')
-    print('encerrando o programa \n')
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print(texto)
+    print()
 
+def escolher_opcao():
+ 
     if opcao_escolhida == 1:
-        print('cadastrar restaurante')
+            print('cadastrar restaurante')
     elif opcao_escolhida == 2:
-        print('listar restaurantes')
+            print('listar restaurantes')
     elif opcao_escolhida == 3:
-        print('ativar restaurantes')
+            print('ativar restaurantes')
     else:
         finalizar_app()
+
+def main():
+   os.sistem('clear')
+   exibir_nome_do_programa()
+   exibir_opcoes()
+   escolher_opcao()
+
+   if __name__ == '__main__':
+       main()
