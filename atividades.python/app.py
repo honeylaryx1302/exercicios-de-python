9e71640346f00d90659b96171261aa9297ed36e6
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-04-18 08:09:43 -0300

    Add files via upload

--- atividades.python/app.py
+++ atividades.python/app.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = ['pizza','xp']
+restaurantes = [{'nome':'restaurante XP','categoria':'alimento','ativo':False},
+                {'nome':'santa','categoria':'carne','ativo':True},
+                {'nome':'CWB','categoria':'sushi','ativo':False}]
 
 def exibir_nome_do_programa():
  print ("""sabor express
@@ -16,28 +18,58 @@ def finalizar_app():
 
 def voltar_ao_menu_principal():
    input('\n Digite uma tecla "enter"para voltar ao menu principal')
-   main()
-           
-     
-opcao_escolhida = int(input('escolha uma opção'))
-#print('você escolheu a opção', opção_escolhida)
-#print(f' vocêescolheu a opção {opção_escolhida}')
+   main()    
+
+def opcao_invalida():
+    print('opcao invalida!\n')
+    voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
     os.system('clear')
+    linha = '*' * (len)(texto)
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
+def cadastrar_novo_resturante():
+   exibir_subtitulo('cadastro de novos restaurantes:')
+   nome_do_restaurante = input('digite o nome do novo restaurante')
+   categoria = input (f'digite a categoria do resturante {nome_do_restaurante}: ')
+   dado_do_restaurante ={'nome':nome_do_restaurante,'categoria':categoria,'ativo':False}
+   restaurantes.append(nome_do_restaurante)
+   print(f'o restaurante{nome_do_restaurante} foi cadastrado com sucesso!')
+   voltar_ao_menu_principal()
+
+def listar_restaurante():
+    exibir_subtitulo('listando os restarantes')
+
+    print(f'{"nome do restaurante".ljust(22)} | {"categoria".ljust(20)} | status')
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+
+    voltar_ao_menu_principal()
+
 def escolher_opcao():
- 
+ try:
+    opcao_escolhida = int(input('escolha uma opcao:'))
+    
     if opcao_escolhida == 1:
-            print('cadastrar restaurante')
+           cadastrar_novo_resturante()
     elif opcao_escolhida == 2:
-            print('listar restaurantes')
+           listar_restaurante()
     elif opcao_escolhida == 3:
             print('ativar restaurantes')
-    else:
+    elif opcao_escolhida == 4:
         finalizar_app()
+    else:
+      opcao_invalida()
+
+ except:
+    opcao_invalida()
 
 def main():
    os.sistem('clear')
