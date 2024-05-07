0e5f0d4a395b8cadb8cd6d5b8931e1a6b36c9b7f
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-05-07 09:26:07 -0300

    Add files via upload

--- atividades.python/app.py
+++ atividades.python/app.py
@@ -9,6 +9,7 @@ def exibir_nome_do_programa():
  output:
  -exibe o nome do programa
  '''
+
 print ("""sabor express
 """)
 def exibir_opcoes():
@@ -16,26 +17,47 @@ def exibir_opcoes():
     output:
     -nos motra as opções como cadastrar,listar,ativar ou sair do restaurante
   '''
+
  print('1. cadastrar restaurante')
  print('2. listar resturante ')
  print('3. ativar restaurante')
  print('4. sair')
 
 def finalizar_app():
-    '''esta opção é responsavel por finalizar o aplicativo
+    '''Esta opção é responsavel por finalizar o aplicativo
     output:
-    - '''
+    -sai do restaurante
+     '''
+
     exibir_subtitulo('finalizar app')
 
 def voltar_ao_menu_principal():
-   input('\n Digite uma tecla "enter"para voltar ao menu principal')
-   main()    
+    '''Essa funcao é reponsavel por voltar ao menu pricipal assim que voce aperta o enter
+    
+    output:
+    -retorna a tela inicial do aplicativo
+    '''
+
+    input('\n Digite uma tecla "enter"para voltar ao menu principal')
+    main()    
 
 def opcao_invalida():
+    '''esta funcao e responsavel por nos mostrar se a opcao é invalida
+    
+    output:
+    -retorna ao menu principal caso a opcao seja invalida
+    '''
+
     print('opcao invalida!\n')
     voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
+    '''Esta funcao é responsavel por exibir o subtitulo
+    
+    output:
+    -organiza os textos e as linhas,organiza as categorias nas linhas
+    '''
+
     os.system('clear')
     linha = '*' * (len)(texto)
     print(linha)
@@ -62,6 +84,12 @@ def cadastrar_novo_resturante():
    voltar_ao_menu_principal()
 
 def listar_restaurante():
+    '''esta funcao é responsavel por lostar o restaurante
+    
+    output:
+    -mostra o nome do restaurante,as categorias e os status do restaurante
+    '''
+
     exibir_subtitulo('listando os restarantes')
 
     print(f'{"nome do restaurante".ljust(22)} | {"categoria".ljust(20)} | status')
@@ -74,8 +102,15 @@ def listar_restaurante():
     voltar_ao_menu_principal()
 
 def alternar_estado_restaurante():
+   '''esta funcao é responsavel por alternaar o estado do restaurante
+     input:nome do restaurante
+    
+   output:
+    -fazer alternacao do estado do restaurante
+   '''
+
    exibir_subtitulo('alternado estado do restaurante')
-   nome_restaurante = input('digite o nome do retaurante que deseja alterar o estado:')
+   nome_restaurante = input('digite o nome do restaurante que deseja alterar o estado:')
    restaurante_encontrado = False
 
    for restaurante in restaurantes:
@@ -90,7 +125,12 @@ def alternar_estado_restaurante():
    voltar_ao_menu_principal()
 
 def escolher_opcao():
- try:
+    '''esta funcao é responsavel por escolher as opcoes 
+    
+    output:
+    -escolha das opcoes
+    '''
+try:
     opcao_escolhida = int(input('escolha uma opcao:'))
     
     if opcao_escolhida == 1:
@@ -104,14 +144,20 @@ def escolher_opcao():
     else:
       opcao_invalida()
 
- except:
+except:
     opcao_invalida()
 
 def main():
-   os.system('clear')
-   exibir_nome_do_programa()
-   exibir_opcoes()
-   escolher_opcao()
+    '''esta funcao é responsavel por garantir que as funcoes sejam executadas quando o programa for iniciado
+    
+    output:
+    -faz com que as funcoes sejam executadas corretamente assim que o programa for iniciado
+    '''
+
+    os.system('clear')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
 if __name__ == '__main__':
     main()
